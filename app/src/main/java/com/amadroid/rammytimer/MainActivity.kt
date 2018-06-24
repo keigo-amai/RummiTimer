5c3bb104f5a77cb70cf4345daadb199f3f5c9a31
keigo.amai <amaikeigo.10@gmail.com>
2018-06-25 02:15:24 +0900

    設定画面を作った

--- app/src/main/java/com/amadroid/rammytimer/MainActivity.kt
+++ app/src/main/java/com/amadroid/rammytimer/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.amadroid.rammytimer
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.amadroid.rammytimer.fragment.SettingFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,11 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,11 +25,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings ->  {
+                startActivity(Intent(this, SettingActivity::class.java))
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
